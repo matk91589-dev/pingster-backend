c591c56a3f5d25763ba389633cf96ad6e012c9ad
matk91589-dev <matk91589@gmail.com>
2026-03-12 01:51:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1672,4 +1672,4 @@ if __name__ == '__main__':
     thread.start()
     
     print("🚀 Сервер запущен на порту 5000")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
